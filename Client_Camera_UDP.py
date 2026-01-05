f9034901e479580e7a1bfe7836ece2f666b1a7f7
기민재 <98635249+bluebeerlolipop@users.noreply.github.com>
2026-01-06 04:44:42 +0900

    Update error messages and comments in Client_Camera_UDP.py

--- Client_Camera_UDP.py
+++ Client_Camera_UDP.py
@@ -9,29 +9,29 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
-    print("❌ 카메라 열기 실패")
+    print("Fail to open Camera")
     exit()
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("❌ 프레임 읽기 실패")
+        print("Fail to read Frame")
         break
 
-    frame = cv2.resize(frame, (320, 240))  # 크기 줄이면 전송량 더 감소
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]  # 품질 낮춰 압축률 ↑
+    frame = cv2.resize(frame, (320, 240))  # frame size
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]  # quality 조절
     result, encimg = cv2.imencode('.jpg', frame, encode_param)
 
     if not result:
-        print("❌ JPEG 인코딩 실패")
+        print("JPEG encoding fail")
         continue
 
     data = encimg.tobytes()
 
-    # 너무 큰 경우 분할
+    # size 분할
     max_packet_size = 4096
     total_packets = len(data) // max_packet_size + 1
-    sock.sendto(str(total_packets).encode(), (UDP_IP, UDP_PORT))  # 패킷 개수 먼저 전송
+    sock.sendto(str(total_packets).encode(), (UDP_IP, UDP_PORT))
 
     for i in range(total_packets):
         part = data[i*max_packet_size:(i+1)*max_packet_size]
